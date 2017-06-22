ae67781ea9b84037d273034a68b66b259d9ab7a7
kushal mahindrakar <kushmahindrakar@gmail.com>
2017-06-22 14:01:55 +0530

    adding value to database using UI ...

--- app/src/main/java/com/kushal/firebasedatabase2auth/MainActivity.java
+++ app/src/main/java/com/kushal/firebasedatabase2auth/MainActivity.java
@@ -9,36 +9,64 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import static android.R.attr.name;
 
 public class MainActivity extends AppCompatActivity {
 
-    private FirebaseDatabase mDB;
+    EditText mEditText;
+    Button mButton;
+    TextView mTextView;
 
+
+    DatabaseReference mRootRef = FirebaseDatabase.getInstance().getReference();
+    DatabaseReference condition = mRootRef.child("name");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
-        mDB = FirebaseDatabase.getInstance();
-        DatabaseReference myRef = mDB.getReference();
-        myRef.child("hello").child("message").child("kushals msg").setValue("hie this is ninty....");
-
-
-
-
+        mEditText = (EditText) findViewById(R.id.editText);
+        mButton = (Button) findViewById(R.id.button2);
+        mTextView = (TextView) findViewById(R.id.fbtxt);
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        condition.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+
+                String name = dataSnapshot.getValue(String.class);
+                mTextView.setText(name);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+               String usersnm = mEditText.getText().toString();
+                condition.setValue(usersnm);
+
+            }
+        });
+    }
 }
-
-
